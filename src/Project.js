dcde4813be18c2d480e1d41124fb479fae712bec
discocisco <f.andresort@gmail.com>
2019-03-20 15:21:39 -0400

    Add project and projects components completed
    - Add screenshots of Scene It project
    - Add project contents in Project.js
    - Add array of project information to be mapped in Projects.js
    - Add styling to Portfolio component

--- src/Project.js
+++ src/Project.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const Project = ({ projectName, projectClass }) => (
-  <div className='project-item'>
+const Project = ({ key, title, projStyling, imgSrc, summary, link, tech, repos }) => (
+  <div className={`project-item ${projStyling}`}>
     <div className='project-photo'>
+      <a href={link} target='_blank' rel='noopener noreferrer'><img src={imgSrc}/></a>
     </div>
     <div className='project-description'>
       <div className='project-title'>
-        {projectName}
+        {title}
+        <div className='project-link'>
+          <a href={link} target='_blank' rel='noopener noreferrer'>{link}</a>
+        </div>
       </div>
       <div className='project-summary'>
+        {summary}
       </div>
       <div className='project-tech'>
+        {tech.map(techItem => <span
+          key={techItem}
+          className={`devicons devicons-${techItem}`}
+        />) }
       </div>
-      <div className='project-links'>
+      <div className='project-repos'>
+        <span className='project-github-repos devicons devicons-github_badge' /> Repositories:
+        {repos.length === 2 ? <Fragment><a href={repos[0]} target='_blank' rel='noopener noreferrer'>Web Client</a> || <a href={repos[1]} target='_blank' rel='noopener noreferrer'>API</a></Fragment> : <a href={repos[0]} target='_blank' rel='noopener noreferrer'> Web Client</a>}
       </div>
     </div>
   </div>
--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,52 @@
 import React from 'react'
 
 import Project from './Project.js'
+import project1 from './img/SceneIt_screenshot.png'
+import project2 from './img/StumblrApp-Page.png'
+import project3 from './img/GlyphsApp-Page.png'
+import project4 from './img/TicTacToeApp-Page.png'
 
 const projectArr = [
-  { name: 'Scene It', project: 'project-1' },
-  { name: 'Stumblr', project: 'project-2' },
-  { name: 'Glyphs', project: 'project-3' },
-  { name: 'Tik-Tak-Toad', project: 'project-4' }
+  { name: 'Scene It',
+    styling: 'project-1',
+    imgSrc: project1,
+    summary: 'Movie database application for storing favorites and writing reviews. Includes Yelp Fusion API to find movie theaters based on location!',
+    link: 'https://discocisco.github.io/scene-it',
+    tech: [
+      'react', 'javascript_badge', 'sass', 'html5', 'css3', 'bootstrap', 'ruby', 'ruby_on_rails', 'postgresql', 'heroku'
+    ],
+    repos: ['https://github.com/discocisco/scene-it', 'https://github.com/discocisco/scene-it-api']
+  },
+  { name: 'Stumblr',
+    styling: 'project-2',
+    imgSrc: project2,
+    summary: 'Microblogging website to share personal blogs for yourself or for your friends! Led a team of four members throughout development of front- and back-end procedures',
+    link: 'https://hex-llc.github.io/stumblr-client',
+    tech: [
+      'javascript_badge', 'sass', 'html5', 'css3', 'bootstrap', 'jquery', 'nodejs', 'mongodb', 'nodejs_small', 'nodejs_small', 'heroku'
+    ],
+    repos: ['https://github.com/hex-llc/stumblr-client', 'https://github.com/hex-llc/stumblr-api']
+  },
+  { name: 'Glyphs',
+    styling: 'project-3',
+    imgSrc: project3,
+    summary: 'Font-viewing tool displaying unique styles in real-time. You can also store your favorites and organize by what project you\'re working on!',
+    link: 'https://discocisco.github.io/glyphs-client',
+    tech: [
+      'javascript_badge', 'sass', 'html5', 'css3', 'bootstrap', 'jquery', 'ruby', 'ruby_on_rails', 'postgresql', 'heroku'
+    ],
+    repos: ['https://github.com/discocisco/glyphs-client', 'https://github.com/discocisco/glyphs-api']
+  },
+  { name: 'Tik-Tak-Toad',
+    styling: 'project-4',
+    imgSrc: project4,
+    summary: 'Classic game of Tic-Tac-Toe with saved profiles and local or online multiplayer! Review past games or resume current ones right where you left off!',
+    link: 'https://discocisco.github.io/tictactoe-client',
+    tech: [
+      'javascript_badge', 'sass', 'html5', 'css3', 'bootstrap', 'jquery'
+    ],
+    repos: ['https://github.com/discocisco/tictactoe-client']
+  }
 ]
 
 const Projects = () => (
@@ -17,9 +57,14 @@ const Projects = () => (
     <div className='project-wrapper'>
       {projectArr.map(project =>
         <Project
-          key={project.project}
-          projectName={project.name}
-          projectClass={project.project}
+          key={project.styling}
+          title={project.name}
+          projStyling={project.styling}
+          imgSrc={project.imgSrc}
+          summary={project.summary}
+          link={project.link}
+          tech={project.tech}
+          repos={project.repos}
         />)}
     </div>
   </div>
