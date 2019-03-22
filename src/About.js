a414b3cfed95df4ede187fe6e78eb392b129c190
discocisco <f.andresort@gmail.com>
2019-03-22 16:23:03 -0400

    Add responsiveness to About component

--- src/About.js
+++ src/About.js
@@ -7,12 +7,12 @@ const About = () => (
     <div className='about-title'>
       About
     </div>
-    <div className='about-profile'>
+    <div className='about-profile container'>
       <div className='about-picture'>
         <img className='about-image' src={profileImage}/>
       </div>
       <div className='about-body'>
-        <p className='about-quote'>{'"Creatively simple code while keeping the user in mind"'}</p>
+        <p className='about-quote'>{'"Turning complex problems into creatively simple solutions"'}</p>
         <p className='about-text'>{'I\'m a developer focused on making the user experience fluid, intuitive, and enjoyable. I enjoy creating applications that are smooth, effective, and UI/UX focused. If I\'m not programming, you can find me building computers, sketching tattoo designs, & researching the latest film practices.'}</p>
       </div>
     </div>
