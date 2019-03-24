39cff97d87a5d78d28afa0f21ddbd13f1ce3e8a6
discocisco <f.andresort@gmail.com>
2019-03-24 15:37:35 -0400

    Adjust About component
    - Remove title for About
    - Center about me content
    - Add new Profile image

--- src/About.js
+++ src/About.js
@@ -1,19 +1,16 @@
 import React from 'react'
 
-import profileImage from './img/profile-photo.png'
+import profileImage from './img/AndresProfileGuac.png'
 
 const About = () => (
   <div id='about' className='about-wrapper'>
-    <div className='about-title'>
-      About
-    </div>
     <div className='about-profile container'>
       <div className='about-picture'>
         <img className='about-image' src={profileImage}/>
       </div>
       <div className='about-body'>
         <p className='about-quote'>{'"Turning complex problems into creatively simple solutions"'}</p>
-        <p className='about-text'>{'I\'m a developer focused on making the user experience fluid, intuitive, and enjoyable. I enjoy creating applications that are smooth, effective, and UI/UX focused. If I\'m not programming, you can find me building computers, sketching tattoo designs, & researching the latest film practices.'}</p>
+        <p className='about-text'>{'I design software that is intuitive and stylized. Other passions include: guacamole, pencil sketching, and building computers.'}</p>
       </div>
     </div>
   </div>
